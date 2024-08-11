7fc4683cb03fceccb93ea03bbc3fbc3288c85153
Sumit9453 <sumit.shkl@gmail.com>
2024-08-11 20:46:33 +0530

    002

--- 002.read.sh
+++ 002.read.sh
@@ -25,7 +25,7 @@ rulev
 
 if [ "$?" = "0" ] ; then
     echo -e "\e[32m Command ran successfully with exit code $?\e[0m"
-elif [ "$1" = "1" ] ; then
+elif [ "$1" = "127" ] ; then
     echo -e "\e[35m Command not executed successfully with exit code $?\e[0m"
 else 
     echo -e "no command executed"
