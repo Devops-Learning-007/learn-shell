93f68db7293696dbbe2fd4fab507af958d71dfee
Sumit9453 <sumit.shkl@gmail.com>
2024-08-11 23:24:37 +0530

    frontend1

--- expense-automation/frontend.sh
+++ expense-automation/frontend.sh
@@ -3,29 +3,7 @@
 ID=$(id -u)
 log="/tmp/frontend.log"
 
-if [ "$ID" -ne 0 ] ; then
-    echo -e "\e[31m Script is expected to be executed as root user or with sudo bash $0\e[0m"
-fi
-
-stat() {
-    if [ $1 -eq 0 ] ; then
-        echo -e "\e[32m - Success\e[0m"
-    else
-        echo -e "\e[31m - Failure\e[0m"   
-    fi
-}
-
-color() {
-    echo -e "\e[35m$*\e[0m"
-}
-
-if [ -f proxy.conf ] ; then
-    color Proxy File Presense
-    stat $?
-else
-    echo -e "\e[31m Proxy file not present, ensure you to suppy\e[0m"
-    exit 1
-fi
+source common.sh
 
 color Installing NGINX
 dnf install nginx -y &>> $log
