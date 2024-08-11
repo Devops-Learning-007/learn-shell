500a7ebb5b833106a5bcfa45e47a59fa5530013d
Sumit9453 <sumit.shkl@gmail.com>
2024-08-11 20:02:22 +0530

    002

--- 002.read.sh
+++ 002.read.sh
@@ -2,7 +2,10 @@
 
 echo "This script will take input from the $(whoami)"
 
-read -p "Enter the value of A:" $A
-read -p "Enter the value of B:" $B
+read -p "Enter the value of A:" A
+read -p "Enter the value of B:" B
+
+echo "Value of A is $A"
+echo "Value of B is $B"
 
 echo "Sum of the A + B = $((A+B))"
